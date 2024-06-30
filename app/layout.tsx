7811a6f3b2e3b6868b8ed01fc9b55995b446c4b1
NazaninShaidi <nazaninshahidi298@gmail.com>
2024-06-30 14:24:44 +0330

    add loading page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
+import { Suspense } from "react";
 import Footer from "./components/layout/Footer";
 import Header from "./components/layout/Header";
 import "./globals.css";
+import Loading from "./loading";
 
 export default function RootLayout({
   children,
@@ -11,7 +13,9 @@ export default function RootLayout({
     <html lang="en">
       <body className="max-w-screen-xl mx-auto">
         <Header />
+        {/* <Suspense fallback={<Loading />}>{children}</Suspense> */}
         {children}
+
         <Footer />
       </body>
     </html>
